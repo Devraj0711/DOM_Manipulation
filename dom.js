9b71841d9739bc09b07b77bece08abc7a719d213
Devraj0711 <singhdevrajtomar7>
2023-05-11 17:05:21 +0530

    first commit

---
+++ dom.js
@@ -0,0 +1,12 @@
+//console.log(document.domain);
+//console.log(document.URL);
+//console.log(document.title);
+//document.title=123;
+//console.log(document.all);
+//console.log(document.all[10]);
+var headerTitle= document.getElementById('main-header');
+headerTitle.style.borderBottom= 'solid 9px #000';
+
+var Titles=document.querySelectorAll('.title');
+Titles[0].style.fontWeight='bold';
+Titles[0].style.color= "green";
